71b5391f738ee1238726aac3c8c23f14dbcb33f8
Mateo <mateo.j.ramallo@gmail.com>
2022-09-01 20:48:45 -0300

    Improves code

--- script_principal.py
+++ script_principal.py
@@ -2,22 +2,16 @@ from Registro import *
 import random
 
 
-def cargar_proyectos():
-    print('-' * 25, 'Cargar Proyectos', '-' * 25)
-
-
 def mostrar_menu():
-    vector_menu = [
-        print('-' * 25, '> Gestión de Proyectos de Software'),
-        print('1. Cargar Proyectos'),
-        print('2. Listar Proyectos'),
-        print('3. Actualizar Proyectos'),
-        print('4. Resumen por Lenguaje'),
-        print('5. Resumen por Año'),
-        print('6. Filtrar Lenguaje'),
-        print('7. Productividad'),
-        print('8. Salir')
-    ]
+    print('-' * 25, '> Gestión de Proyectos de Software')
+    print('1. Cargar Proyectos')
+    print('2. Listar Proyectos')
+    print('3. Actualizar Proyectos')
+    print('4. Resumen por Lenguaje')
+    print('5. Resumen por Año')
+    print('6. Filtrar Lenguaje')
+    print('7. Productividad')
+    print('8. Salir')
 
 
 def convert_tuple_str(tuple):
@@ -27,8 +21,36 @@ def convert_tuple_str(tuple):
     return tuple_str
 
 
-def crear_registros(id, titulo, fecha_actualizacion, lenguaje, lineas_codigo):
-    pass
+def ordenar_titulo(vec):
+    vec_alf = []
+    # Acceder al casillero de los titulos en el vector de registros
+    for i in range(len(vec)):
+        # Almacenar todos los titulos de los proyectos en el vector
+        vec_alf.append(vec[i].titulo)
+    # Una vez que ya almacenamos todos los titulos en el vector lo ordenamos
+
+    return vec_alf
+
+
+def cargar_proyectos(id, lenguaje, clase=Registro):
+    # Le solicitamos al usuario el titulo del proyecto
+    titulo_proyecto = input('-> Ingrese el titulo del Proyecto a Registrar:')
+    if titulo_proyecto == "":
+        titulo_proyecto = input('-> Ingrese el titulo CORRECTAMENTE del Proyecto a Registrar: ')
+
+    # Solicitamos fecha
+    fecha_proyecto = input('-> Ingrese la fecha de actualización del Proyecto(dd-mm-yyyy): ')
+    while validar_fecha(fecha_proyecto) == False:
+        fecha_proyecto = input(
+            '-> Ingrese la fecha de actualización del Proyecto(dd-mm-yyyy), entre los años 2000 y 2022: ')
+    # Solicitamos Cantidad de lineas de codigo
+    cant_lineas = int(input('-> Ingrese la cantidad de lineas de codigo del Proyecto a Registrar:'))
+    if cant_lineas <= 0:
+        cant_lineas = int(
+            input('-> Ingrese un Valor CORRECTO en la cantidad de lineas de codigo del Proyecto a Registrar:'))
+
+    reg = Registro(id, titulo_proyecto, fecha_proyecto, lenguaje, cant_lineas)
+    return reg
 
 
 def validar_fecha(fecha):
@@ -53,35 +75,28 @@ def principal():
     id_proyecto = 1
     lenguajes = ('Python', 'Java', 'C++', 'Javascript', 'Shell', 'HTML', 'Ruby', 'Swift', 'C#', 'VB', 'Go')
     registros_vec = []
-    print('Su opcion es:', opcion)
+    # print('Su opcion es:', opcion)
 
     while opcion != 8:
         if opcion == 1:
             print('*' * 25, 'Cargar Proyectos', '*' * 25)
-            leng_elegido = random.choice(lenguajes)
-            titulo_proyecto = input('-> Ingrese el titulo del Proyecto a Registrar:')
-            if titulo_proyecto == "":
-                titulo_proyecto = input('-> Ingrese el titulo CORRECTAMENTE del Proyecto a Registrar: ')
-
-            fecha_proyecto = input('-> Ingrese la fecha de actualización del Proyecto(dd-mm-yyyy): ')
-            while validar_fecha(fecha_proyecto) == False:
-                fecha_proyecto = input(
-                    '-> Ingrese la fecha de actualización del Proyecto(dd-mm-yyyy), entre los años 2000 y 2022: ')
-
-            cant_lineas = int(input('-> Ingrese la cantidad de lineas de codigo del Proyecto a Registrar:'))
-            if cant_lineas <= 0:
-                cant_lineas = int(
-                    input('-> Ingrese un Valor CORRECTO en la cantidad de lineas de codigo del Proyecto a Registrar:'))
-
-            # Generamos la instancia de la clase Registro
-            registro = Registro(id_proyecto, titulo_proyecto, fecha_proyecto, leng_elegido, cant_lineas)
-
-            #Incrementamos el contador de id_proyecto
+            # Generamos el lenguaje aleatoriamente
+            id_lenguaje = random.randint(0, 10)
+            # Incrementamos el contador de id_proyecto
             id_proyecto += 1
-
-            #Lo agregamos al vector de registros
+            # LLamamos a la funcion crear registro para poder generar una instancia de nuestra clase Registro
+            registro = cargar_proyectos(id_proyecto, lenguajes[id_lenguaje])
+            # Lo agregamos al vector de registros
             registros_vec.append(registro)
+        elif opcion == 2:
+            # ordenar_titulo(registros_vec)
+            print('Mostramos el registro generado')
+            for i in range(len(registros_vec)):
+                print(registros_vec[i])
+            pass
 
+        # Mostramos nuevamente el menu
+        mostrar_menu()
         opcion = int(input('Ingrese su opcion: '))
 
 
